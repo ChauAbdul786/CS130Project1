0d5a6b10a487ee45cc7782a7e39c2174d50ab88d
ChauAbdul786 <ChauAbdul786@gmail.com>
2021-10-12 19:14:01 -0700

    Initializing Directory

---
+++ grading/sphere.cpp
@@ -0,0 +1,62 @@
+#include "sphere.h"
+#include "ray.h"
+
+// Determine if the ray intersects with the sphere
+Hit Sphere::Intersection(const Ray& ray, int part) const
+{   //(a) t^2 * dot(B, B) + 
+    //(b) 2t * dot(B, A - C) + 
+    //(c) dot(A - C, A - C) - r^2
+    //Where A = vec endpoint
+    //      B = direction vector
+    //      C = center of sphere
+
+    double a = dot(ray.direction, ray.direction);
+    double b = 2 * dot(ray.direction, (ray.endpoint - center));
+    double c = dot((ray.endpoint - center), (ray.endpoint - center)) - (radius * radius);
+    Hit h;
+
+    double t0 = -1;
+    double t1 = -1;
+
+    double discriminant = ((b * b) - (4 * (a * c)));
+    if(discriminant >= 0){ //There is an intersection with the sphere
+        t0 = (sqrt(discriminant) - b) / (2 * a); //Calculate the (potentially) 2 intersection points w the sphere
+        t1 = ((0 - b) - sqrt(discriminant)) / (2 * a);
+    }
+
+    if(t0 < small_t){ //Check if either intersection is < small_t
+        t0 = -1;
+    }
+
+    if(t1 < small_t){
+        t1 = -1;
+    }
+
+    if((t0 > 0) || (t1 > 0)){ //Check that either of the intersection points are > small_t
+        h.object = this;
+    }
+
+    if(t0 > 0 && t1 > 0){ //Intersection through the sphere
+        h.dist = std::min(t0, t1); //Distance is the closer intersection point of the two
+    }else if(t0 == t1){ //Intersection is tangent to sphere
+        h.dist = t0;
+    }
+
+    
+    return h;
+}
+
+vec3 Sphere::Normal(const vec3& point, int part) const
+{
+    vec3 normal;
+    TODO; // compute the normal direction
+    return normal;
+}
+
+Box Sphere::Bounding_Box(int part) const
+{
+    Box box;
+    TODO; // calculate bounding box
+    return box;
+}
+
---
+++ render_world.cpp
@@ -0,0 +1,84 @@
+#include "render_world.h"
+#include "flat_shader.h"
+#include "object.h"
+#include "light.h"
+#include "ray.h"
+
+extern bool disable_hierarchy;
+
+Render_World::Render_World()
+    :background_shader(0),ambient_intensity(0),enable_shadows(true),
+    recursion_depth_limit(3)
+{}
+
+Render_World::~Render_World()
+{
+    delete background_shader;
+    for(size_t i=0;i<objects.size();i++) delete objects[i];
+    for(size_t i=0;i<lights.size();i++) delete lights[i];
+}
+
+// Find and return the Hit structure for the closest intersection.  Be careful
+// to ensure that hit.dist>=small_t.
+Hit Render_World::Closest_Intersection(const Ray& ray)
+{
+    double min_t = std::numeric_limits<double>::infinity();
+    Hit h;
+    Hit closest_hit;
+    for(auto o: objects){
+        h = o->Intersection(ray, -1);
+        if(h.object){
+            if((h.dist < (min_t && h.dist)) >= small_t){
+                closest_hit = h;
+                min_t = h.dist;
+            }
+        }
+    }
+    return {};
+}
+
+// set up the initial view ray and call
+void Render_World::Render_Pixel(const ivec2& pixel_index)
+{
+    vec3 dir_vec = (this->camera.World_Position(pixel_index)) - (this->camera.position);
+    vec3 normalized_dir_vec = dir_vec.normalized();
+    Ray ray(this->camera.position, normalized_dir_vec);
+    vec3 color=Cast_Ray(ray,1);
+    camera.Set_Pixel(pixel_index,Pixel_Color(color));
+}
+
+void Render_World::Render()
+{
+    if(!disable_hierarchy)
+        Initialize_Hierarchy();
+
+    for(int j=0;j<camera.number_pixels[1];j++)
+        for(int i=0;i<camera.number_pixels[0];i++)
+            Render_Pixel(ivec2(i,j));
+}
+
+// cast ray and return the color of the closest intersected surface point,
+// or the background color if there is no object intersection
+vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth)
+{
+    vec3 color;
+    Hit h = Closest_Intersection(ray);
+    if(h.object){ //There was an intersection with an object
+        vec3 interesection_point = ray.Point(h.dist); //Point on the ray distance "h.dist" away 
+        vec3 normal = (h.object)->Normal(interesection_point, h.part);
+
+        color = h.object->material_shader->Shade_Surface(ray, interesection_point, normal, recursion_depth);
+    }else{ //There was not an intersection with an object
+        color = background_shader->Shade_Surface(ray, {}, {}, recursion_depth);
+    }
+    return color;
+}
+
+void Render_World::Initialize_Hierarchy()
+{
+    TODO; // Fill in hierarchy.entries; there should be one entry for
+    // each part of each object.
+
+    hierarchy.Reorder_Entries();
+    hierarchy.Build_Tree();
+}
