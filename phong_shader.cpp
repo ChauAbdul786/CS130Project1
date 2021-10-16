02830590630a6ea4fd727625e7103619db397748
ChauAbdul786 <ChauAbdul786@gmail.com>
2021-10-16 01:42:15 +0100

    Added Ambient and Diffuse Shading

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -9,6 +9,23 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
+    vec3 ambient;
+    vec3 diffuse;
+
+    //Ambient
+    ambient = color_ambient;
+
     TODO; //determine the color
+    for(unsigned i = 0; i < world.lights.size(); i++){ //For every color in the world space
+        vec3 l = world.lights[i]->position - intersection_point; //Calculate l for each light 
+
+    //Diffuse Id = (Rd)(Ld)(max(dot(n ,l), 0))
+    diffuse = color_diffuse * world.lights[i]->Emitted_Light(l) * std::max(dot(normal, l.normalized()), 0.00);
+
+    //Specular TODO
+
+
+    }
+    color = ambient + diffuse;
     return color;
 }
